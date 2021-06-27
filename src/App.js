2a6203f3c84c1c02e19f0a5596a8acc44c6ac5a8
Son Quach <sonqhb@gmail.com>
2021-06-27 19:14:58 +0700

    Design trello font-end

--- src/App.js
+++ src/App.js
@@ -1,13 +1,113 @@
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-        <h1 className="hello-world">
-          Quach Son
-        </h1>
+    <div className="trello-main">
+        <nav className="navbar app">
+          App Bar
+        </nav>
+        <nav className="navbar board">
+        board Bar
+        </nav>
+        <div className="board-columns">
+          <div className="column">
+            <header>Brainstorm</header>
+            <ul>
+              <li>
+                <img src="https://loremflickr.com/320/240/dog" alt="sonquachdev" />
+                Title: SonQuachDev
+              </li>
+              <li>Add what you'd like to work on below</li>
+              <li>Add what you'd like to work on below</li>
+              <li>Add what you'd like to work on below</li>
+              <li>Add what you'd like to work on below</li>
+              <li>Add what you'd like to work on below</li>
+              <li>Add what you'd like to work on below</li>
+              <li>Add what you'd like to work on below</li>
+              
+            </ul>
+            <footer>Add another card</footer>
+          </div>
+          <div className="column">
+            <header>Brainstorm</header>
+            <ul>
+              <li>
+                <img src="https://loremflickr.com/320/240/dog" alt="sonquachdev" />
+                Title: SonQuachDev
+              </li>
+              <li>Add what you'd like to work on below</li>
+              <li>Add what you'd like to work on below</li>
+              <li>Add what you'd like to work on below</li>
+              <li>Add what you'd like to work on below</li>
+              <li>Add what you'd like to work on below</li>
+            </ul>
+            <footer>Add another card</footer>
+          </div>
+          <div className="column">
+            <header>Brainstorm</header>
+            <ul>
+              <li>
+                <img src="https://loremflickr.com/320/240/dog" alt="sonquachdev" />
+                Title: SonQuachDev
+              </li>
+              <li>Add what you'd like to work on below</li>
+              <li>Add what you'd like to work on below</li>
+              <li>Add what you'd like to work on below</li>
+              <li>Add what you'd like to work on below</li>
+              <li>Add what you'd like to work on below</li>
+            </ul>
+            <footer>Add another card</footer>
+          </div>
+          <div className="column">
+            <header>Brainstorm</header>
+            <ul>
+              <li>
+                <img src="https://loremflickr.com/320/240/dog" alt="sonquachdev" />
+                Title: SonQuachDev
+              </li>
+              <li>Add what you'd like to work on below</li>
+              <li>Add what you'd like to work on below</li>
+              <li>Add what you'd like to work on below</li>
+              <li>Add what you'd like to work on below</li>
+              <li>Add what you'd like to work on below</li>
+            </ul>
+            <footer>Add another card</footer>
+          </div>
+          <div className="column">
+            <header>Brainstorm</header>
+            <ul>
+              <li>
+                <img src="https://loremflickr.com/320/240/dog" alt="sonquachdev" />
+                Title: SonQuachDev
+              </li>
+              <li>Add what you'd like to work on below</li>
+              <li>Add what you'd like to work on below</li>
+              <li>Add what you'd like to work on below</li>
+              <li>Add what you'd like to work on below</li>
+              <li>Add what you'd like to work on below</li>
+            </ul>
+            <footer>Add another card</footer>
+          </div>
+          <div className="column">
+            <header>Brainstorm</header>
+            <ul>
+              <li>
+                <img src="https://loremflickr.com/320/240/dog" alt="sonquachdev" />
+                Title: SonQuachDev
+              </li>
+              <li>Add what you'd like to work on below</li>
+              <li>Add what you'd like to work on below</li>
+              <li>Add what you'd like to work on below</li>
+              <li>Add what you'd like to work on below</li>
+              <li>Add what you'd like to work on below</li>
+            </ul>
+            <footer>Add another card</footer>
+          </div>
+        </div>
+       
     </div>
   );
 }
 
 export default App;
+
