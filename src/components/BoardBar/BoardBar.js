1a21f3d8a8eb901791b041db110f1624573f686c
Son Quach <sonqhb@gmail.com>
2021-06-30 01:00:00 +0700

    Clean React Architecture

---
+++ src/components/BoardBar/BoardBar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './BoardBar.scss'
+
+function BroadBar(){
+    return (
+        <nav className="navrbar-board">Broad Bar</nav>
+        )
+}
+
+export default BroadBar
---
+++ src/components/Task/Task.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import './Task.scss'
+
+function Task(){
+    return (
+        
+              <li className="Task-item">
+                <img src="https://loremflickr.com/320/240/dog" alt="sonquachdev" />
+                Title: SonQuachDev
+                
+              </li>
+             
+        )
+}
+
+export default Task
