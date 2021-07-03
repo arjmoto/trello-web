1935530fa70625758fc15ec768e75f9099f33c31
SonQH <sonqhb@gmail.com>
2021-07-03 16:36:21 +0700

    Implement drap and drop , eslint

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import './App.scss';
+import React from 'react'
+import './App.scss'
+
 // custom components
-import AppBar from 'components/AppBar/AppBar';
-import BoardBar from 'components/BoardBar/BoardBar';
-import BoardContent from 'components/BoardContent/BoardContent';
+import AppBar from 'components/AppBar/AppBar'
+import BoardBar from 'components/BoardBar/BoardBar'
+import BoardContent from 'components/BoardContent/BoardContent'
 function App() {
   return (
+
     <div className="trello-main">
+
       <AppBar />
       <BoardBar/>
-      <BoardContent/> 
+      <BoardContent/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
 
--- src/components/BoardContent/BoardContent.js
+++ src/components/BoardContent/BoardContent.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react'
-import { isEmpty} from 'lodash'
+import { Container, Draggable } from 'react-smooth-dnd'
+import { isEmpty } from 'lodash'
 import './BoardContent.scss'
 import './BoardContent.scss'
-import Column from 'components/Column/Column';
+import Column from 'components/Column/Column'
 import { mapOrder } from 'utilities/sorts'
 import { initialData } from 'actions/initialData'
 
-function BoardContent(){
+function BoardContent() {
   const [board, setBoard] = useState({})
   const [columns, setColumns] = useState([])
   useEffect(() => {
@@ -17,15 +18,36 @@ function BoardContent(){
       setColumns(mapOrder(boardFromDB.columns, boardFromDB.columnOrder, 'id'))
     }
   }, [])
-  if (isEmpty(board)){
-    return <div className="not-found" style = {{ 'padding': '10px', 'color': 'white'}}>Board not found</div>
+  if (isEmpty(board)) {
+    return <div className="not-found" style = {{ 'padding': '10px', 'color': 'white' }}>Board not found</div>
   }
-    return (
-        <div className="board-content">
-          {columns.map((column, index)=> <Column key = {index} column = {column}/> )}
-          
-        </div>
-        )
+  const onColumnDrop = (dropResult) => {
+    // eslint-disable-next-line no-console
+    console.log(dropResult)
+  }
+  return (
+    <div className="board-content">
+      <Container
+        orientation="horizontal"
+        onDrop={onColumnDrop}
+        getChildPayload={index => columns[index]}
+        dragHandleSelector=".column-drag-handle"
+        dropPlaceholder={{
+          animationDuration: 150,
+          showOnTop: true,
+          className: 'column-drop-preview'
+        }}
+      >
+        {columns.map((column, index) => (
+          <Draggable key = {index}>
+            <Column column = {column}/>
+          </Draggable>
+        ) )}
+      </Container>
+
+
+    </div>
+  )
 }
 
 export default BoardContent
