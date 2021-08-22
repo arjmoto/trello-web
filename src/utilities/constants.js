fcd223e66ba7ad6d31b38c8d702346a50ce36601
Son Quach <sonqhb@gmail.com>
2021-08-22 14:57:25 +0700

    Deploy trello web to firebase.

--- src/utilities/constants.js
+++ src/utilities/constants.js
@@ -1,3 +1,5 @@
-export const API_ROOT = 'http://localhost:9184'
+// export const API_ROOT = 'http://localhost:9184'
+export const API_ROOT = 'https://trello-sonquach-api.herokuapp.com'
+
 export const MODAL_ACTION_CLOSE = 'MODAL_ACTION_CLOSE'
 export const MODAL_ACTION_CONFIRM = 'MODAL_ACTION_CONFIRM'
